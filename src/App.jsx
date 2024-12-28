fc6f52789bf80228af973e3dc3e37e9d6b7a7941
khush-patel-001 <khushpatel0030@gmail.com>
2024-12-28 16:03:31 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import ContactPage from "./Pages/ContactPage/ContactPage";
-import NoPage from "./Pages/NoPage/NoPage";
 import CodeEditor from "./Pages/CodeEditor/CodeEditor";
 import FrontendEditor from "./Pages/FrontendEditor/FrontendEditor";
 import {
@@ -20,7 +19,6 @@ function App() {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/frontend-editor" element={<FrontendEditor />} />
           <Route path="/editor" element={<CodeEditor />} />
-          <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </>
