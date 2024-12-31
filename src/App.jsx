58c9e606392d5147d0e274b54f9bd50b7a8f232c
khush-patel-001 <khushpatel0030@gmail.com>
2024-12-31 19:30:09 +0530

    Prepare for github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import ContactPage from "./Pages/ContactPage/ContactPage";
 import CodeEditor from "./Pages/CodeEditor/CodeEditor";
 import FrontendEditor from "./Pages/FrontendEditor/FrontendEditor";
 import { Routes, Route, HashRouter } from "react-router-dom";
+window.process = window.process || { env: { PUBLIC_URL: "" } };
 function App() {
   return (
     <>
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
