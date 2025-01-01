ba471b42cccc9dff93c5cd1b998cba97e521ea41
khush-patel-001 <khushpatel0030@gmail.com>
2025-01-01 17:10:26 +0530

    add deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import ContactPage from "./Pages/ContactPage/ContactPage";
 import CodeEditor from "./Pages/CodeEditor/CodeEditor";
 import FrontendEditor from "./Pages/FrontendEditor/FrontendEditor";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-window.process = window.process || { env: { PUBLIC_URL: "" } };
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/frontend-editor" element={<FrontendEditor />} />
